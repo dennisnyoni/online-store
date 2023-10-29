2d4d3e115676cf1fc263378dc6d1ad9fd8d76a87
dennisnyoni <dennisnyoni@rocketmail.com>
2023-10-28 21:27:29 -0500

    Add files via upload

---
+++ environment.ts
@@ -0,0 +1,18 @@
+export const environment = {
+  production: false,
+  stripe: {
+
+    publicKey: 'pk_test_51O2JjLDkFvXdLHNcmllved2zdzZXFymVNV0ip0lwEwN1dGfvdTlng8eNwH9X3q6UdNSqnPKPJJKP8D9sXVcvmUS000yCwKKpal',
+    testKey: 'sk_test_51O2JjLDkFvXdLHNciIbHLBm6Ie5X4NucjaFq10Dunfwo5tbPk4cJfYYyh0voJ5ayybQKcmC6TWJLgOCD1ScaoiW700jMfVdOXi'
+  },
+  paymentApiUrl:'http://10.200.31.152:8889/payment-service/payments',
+  authServiceApiUrl : 'http://localhost:8889/validation',
+  clientBaseUrl: '',
+  orderBaseUrl:'http://10.200.31.152:8889/order-service/cart',
+  paymentBaseUrl:'http://10.200.31.152:8889/order-service/order',
+  productBaseUrl: 'http://10.200.31.152:8889/product-service/products',
+  shoppingCartBaseUrl: '',
+  userBaseUrl: 'http://10.200.28.22:8881/users',
+  vendorBaseUrl:'',
+  shipInBaseUrl:'http://10.200.28.22:8881/product-service/products'
+};
